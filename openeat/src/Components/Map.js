b728f5fa8bfd99d1dd666ef4c0e51a890eac396f
mtruvo <137792515+Uvomtr@users.noreply.github.com>
2024-01-21 18:38:26 +0800

    adding maps fixed position

--- openeat/src/Components/Map.js
+++ openeat/src/Components/Map.js
@@ -1,27 +1,29 @@
 // Map.js
-
 import React from "react";
 import { MapContainer, TileLayer, Rectangle } from "react-leaflet";
+import "leaflet/dist/leaflet.css"; // Import the Leaflet CSS for styling
 
 const Map = () => {
-  const centerOfPhilippines = [12.8797, 121.774]; // Center of the Philippines
+  const centerOfManila = [14.5995, 120.9842]; // Center of Manila, Philippines
   const fenceBounds = [
     [19.41, 116.8],
     [4.64, 126.6],
   ]; // Coordinates of the Philippines
 
   return (
-    <MapContainer
-      center={centerOfPhilippines}
-      zoom={6}
-      className="map-container"
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Rectangle bounds={fenceBounds} color="red" />
-    </MapContainer>
+    <div className="map-container">
+      <MapContainer
+        center={centerOfManila}
+        zoom={12}
+        style={{ height: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Rectangle bounds={fenceBounds} color="red" />
+      </MapContainer>
+    </div>
   );
 };
 
