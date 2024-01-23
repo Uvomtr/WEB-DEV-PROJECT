91b4550e5c54ac3c1aae8d21cec9bdd150230e1c
tinnamaan <131854078+tinnamaan02@users.noreply.github.com>
2024-01-24 03:55:41 +0800

    Update Footer.js

--- openeat/src/Components/Footer.js
+++ openeat/src/Components/Footer.js
@@ -21,18 +21,18 @@ const Footer = () => {
       </div>
       <div className="footer-section-two">
         <div className="footer-section-columns">
-          <span>Qualtiy</span>
+          <span>Quality</span>
           <span>Help</span>
           <span>Share</span>
-          <span>Carrers</span>
+          <span>Careers</span>
           <span>Testimonials</span>
           <span>Work</span>
         </div>
         <div className="footer-section-columns">
-          <span>244-5333-7783</span>
-          <span>hello@food.com</span>
-          <span>press@food.com</span>
-          <span>contact@food.com</span>
+          <span></span>
+          <span>openeat.it@gmail.com</span>
+          <span></span>
+          <span></span>
         </div>
         <div className="footer-section-columns">
           <span>Terms & Conditions</span>
