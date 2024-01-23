feac320b038400965bbfff2abfa1e7c9c1fc14d6
aliahyaba <aliahmhae.yaba@tup.edu.ph>
2024-01-24 00:51:14 +0800

    Updated Home.js

--- openeat/src/Components/Home.js
+++ openeat/src/Components/Home.js
@@ -18,8 +18,11 @@ const Home = () => {
               Your Favourite Food Delivered Hot & Fresh
             </h1>
             <p className="primary-text">
-              Healthy switcher chefs do all the prep work, like peeding,
-              chopping & marinating, so you can cook a fresh food.
+              Our user-friendly interface lets you personalize your culinary
+              journey with intuitive filters, allowing you to exclude unwanted
+              cuisines or ingredients. Elevate your dining adventures by
+              exploring a tailored selection that aligns seamlessly with your
+              palate.
             </p>
             <a href="#menu-section" className="secondary-button">
               Order Now <FiArrowRight />
