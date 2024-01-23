b4979a513c7502228b189544511cf596a1af01d4
aliahyaba <aliahmhae.yaba@tup.edu.ph>
2024-01-24 01:28:44 +0800

    Update Testimonial.js

--- openeat/src/Components/Testimonial.js
+++ openeat/src/Components/Testimonial.js
@@ -90,7 +90,7 @@ const Testimonial = () => {
                 <AiFillStar />
                 <AiFillStar />
               </div>
-              <h2>Aliah Mhae</h2>
+              <h2>Aliah Mhae F. Yaba</h2>
             </div>
           </div>
         </div>
