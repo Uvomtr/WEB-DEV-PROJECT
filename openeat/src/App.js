12655b1bc44d32ff5e59f0b28d5c6db6e08d8e97
mtruvo <137792515+Uvomtr@users.noreply.github.com>
2024-01-24 13:07:31 +0800

    Update App.js

--- openeat/src/App.js
+++ openeat/src/App.js
@@ -3,7 +3,9 @@ import Home from "./Components/Home";
 import About from "./Components/About";
 import Menu from "./Components/Menu";
 import Work from "./Components/Work";
-
+import Testimonial from "./Components/Testimonial";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 import Map from "./Components/Map"; // Import the Map component
 
 function App() {
