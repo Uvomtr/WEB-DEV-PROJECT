67cfc5b38e1e43fd8a26eefc2bec920d92fbd98c
mtruvo <137792515+Uvomtr@users.noreply.github.com>
2024-01-24 14:45:50 +0800

    Update Map.js

--- openeat/src/Components/Map.js
+++ openeat/src/Components/Map.js
@@ -14,19 +14,18 @@ const Map = () => {
   const fenceBounds = [
     [19.41, 116.8],
     [4.64, 126.6],
-  ]; // Coordinates of the Philippines
+  ];
 
   const [restaurants, setRestaurants] = useState([]);
 
   useEffect(() => {
-    // Fetch nearby restaurants using an API (replace with your preferred API)
     fetchNearbyRestaurants(centerOfManila[0], centerOfManila[1])
       .then((data) => setRestaurants(data))
       .catch((error) => console.error("Error fetching restaurants:", error));
   }, [centerOfManila]);
 
   const fetchNearbyRestaurants = async (lat, lng) => {
-    const radius = 5000; // Set the radius to 5 kilometers (adjust as needed)
+    const radius = 5000;
     const url = `https://api.openstreetmap.org/api/0.6/map?bbox=${lng - 0.05},${
       lat - 0.05
     },${lng + 0.05},${lat + 0.05}`;
