9dfd1814b0d90f9c45fe1b0874ac698ad43131b9
mtruvo <137792515+Uvomtr@users.noreply.github.com>
2024-01-25 10:08:48 +0800

    Update App.js

--- openeat/src/App.js
+++ openeat/src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
-import Menu from "./Components/Menu";
+import Menu from "./Components/Restaurant";
 import Work from "./Components/Work";
 import Testimonial from "./Components/Testimonial";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 import Map from "./Components/Map"; // Import the Map component
+import Restaurant from "./Components/Restaurant";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
-      <Menu />
+      <Restaurant />
       <Work />
       <Testimonial />
       <Contact />
